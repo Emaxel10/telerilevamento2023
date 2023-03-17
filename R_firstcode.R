dedf43db0b7605c1ab9d9c429a1149062f586817
Emaxel10 <127427373+Emaxel10@users.noreply.github.com>
2023-03-17 17:50:24 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -52,3 +52,16 @@ plot(l2011[[3]], col=clr)
 # nir
 clnir <- colorRampPalette(c("darkgoldenrod", "orange", "blueviolet")) (100)
 plot(l2011[[4]], col=clnir)
+
+# RGB plotting
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")
+
+# Multiframe with natural and false colours
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(l2011, r=3, g=2, b=4, stretch="Lin")
+
+# Histogram stratching
+par(mfrow = c(2,1))
+plotRGB(l2011, r=3, g=2, b=1, stretch="Hist")
+plotRGB(l2011, r=3, g=2, b=4, stretch="Hist")
