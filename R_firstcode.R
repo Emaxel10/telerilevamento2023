3af93c25aac7ab1e4173166382be6709183dce7e
Emaxel10 <127427373+Emaxel10@users.noreply.github.com>
2023-03-23 17:38:33 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -63,5 +63,25 @@ plotRGB(l2011, r=3, g=2, b=4, stretch="Lin")
 
 # Histogram stratching
 par(mfrow = c(2,1))
-plotRGB(l2011, r=3, g=2, b=1, stretch="Hist")
-plotRGB(l2011, r=3, g=2, b=4, stretch="Hist")
+plotRGB(l2011, 4, 3, 2, stretch="Hist")
+
+# Excersise: import the 1988 image
+l1988 <- brick("p224r63_1988_masked.grd")
+l1988
+plot(l1988)
+
+plotRGB(l1988, r=3, g=2, b=1, stretch="Lin")
+plotRGB(l1988, r=4, g=3, b=2, stretch="Lin")
+
+# Multiframe
+par(mfrow = c(2,1))
+plotRGB(l1988, 4, 3, 2, stretch="Lin")
+plotRGB(l2011, 4, 3, 2, stretch="Lin")
+
+# Esercizio, fare un multiframe 2 righe e 2 colonne, 1988 lin, 2011 lin, 1988 hist, 2011 hist
+# RGB con infrarosso in red
+par(mfrow = c(2,2))
+plotRGB(l1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(l1988, r=4, g=3, b=2, stretch="Hist")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Hist")
