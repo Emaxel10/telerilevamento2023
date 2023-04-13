3bfd71adfc3391d0708fb24a48cdfca2062c2515
Emaxel10 <127427373+Emaxel10@users.noreply.github.com>
2023-04-13 17:47:04 +0200

    Create R_code_classification.R

---
+++ R_code_classification.R
@@ -0,0 +1,6 @@
+# Classification of remote sensing via RStoolBox
+# Installing devtools
+install.packages("devtools")
+library(devtools)
+devtools::install_github("bleutner/RStoolbox")
+setwd("C:/lab/")
