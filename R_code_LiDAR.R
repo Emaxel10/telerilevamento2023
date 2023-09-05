2f269253c72393c2de5bee647d11ad5147d5a4e5
Emaxel10 <127427373+Emaxel10@users.noreply.github.com>
2023-09-05 21:44:38 +0200

    Update R_code_LiDAR.R

--- R_code_LiDAR.R
+++ R_code_LiDAR.R
@@ -1,33 +1,34 @@
 # 3D in R
 
+# Set up libraries and wd
 setwd("C:/Lab/dati")
 library(raster)
 library(ggplot2)
 library(viridis)
 library(patchwork)
 
-#importo l'immagine
+# Import the image
 dsm_2013 <- raster("2013Elevation_DigitalElevationModel-0.5m.tif")
 dsm_2013
 
-#creo un dataframe
+# Create a dataframe
 dsm_2013d <- as.data.frame(dsm_2013, xy=T)
 head(dsm_2013d)
 
-#visualizziamo con ggplot
+# Plot via ggplot2
 ggplot() +
   geom_raster(dsm_2013d, mapping=aes(x=x, y=y, fill=X2013Elevation_DigitalElevationModel.0.5m)) +
   scale_fill_viridis() +
   ggtitle("dsm 2013")
 
-#creiamo adesso il modello del terreno
+# Let's now create the terrain model
 dtm_2013 <- raster("2013Elevation_DigitalTerrainModel-0.5m.tif")
 dtm_2013
 
 dtm_2013d <- as.data.frame(dtm_2013, xy=T)
-head(dtm_2013d)
+head(dtm_2013d) # The head() function in R is used to display the first n rows present in the input data frame
 
-#posso rinominare
+# Now we can rename and plot it
 names(dtm_2013d) <- c("x", "y", "z")
 
 ggplot()+
@@ -50,6 +51,6 @@ p3 <-ggplot()+
   scale_fill_viridis() +
   ggtitle("dtm 2013")
 
-#con patchwork
+# With patchwork
 p1 + p2 + p3
 
