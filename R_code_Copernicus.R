de67a383b816e814a9f1c4a0fff7975eca8f0824
Emaxel10 <127427373+Emaxel10@users.noreply.github.com>
2023-09-05 21:48:25 +0200

    Update R_code_Copernicus.R

--- R_code_Copernicus.R
+++ R_code_Copernicus.R
@@ -1,11 +1,13 @@
 # R code for downloading and visualizing Copernicus data
 
 # install.packages("ncdf4")
+# Set up libraries
 library(ncdf4)
 library(raster)
 library(ggplot2)
 library(viridis)
 
+# Set wd
 setwd("C:/lab/")
 
 # dataset: c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc
@@ -13,11 +15,13 @@ setwd("C:/lab/")
 sc <- raster("c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc")
 plot(sc)
 
-# with the raster package -> RasterLayer
-# with the terra package -> SpatRaster: it maintains the uncertainty data, so: 2 layers
+# With the raster package -> RasterLayer
+# With the terra package -> SpatRaster: it maintains the uncertainty data, so: 2 layers
 
+# Create a dataframe
 scd <- as.data.frame(sc, xy=T)
 
+# Plot via ggplot2
 ggplot() +
 geom_raster(scd, mapping=aes(x=x, y=y, fill=Surface.Soil.Moisture)) +
 ggtitle("Soil Moisture from Copernicus")
