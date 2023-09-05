c65800cf1c014d37c9e16fd54f5cfcb8ef2fbe41
Emaxel10 <127427373+Emaxel10@users.noreply.github.com>
2023-09-05 18:26:18 +0200

    Update R_code_variability.R

--- R_code_variability.R
+++ R_code_variability.R
@@ -1,48 +1,62 @@
+# Variability
+
+# Set the working durectory and the libraries
 setwd("C:/lab/")
 library(raster)
 library(ggplot2)
-install.packages("viridis")
-library(viridis)
 library(patchwork)
+
+# Let's install viridis package for different color maps
+install.packages("viridis")
+
+# Import the image
 sen <- brick("sentinel.png")
+
 # Band 1 = NIR
 # Band 2 = red
 # Band 3 = green
+
+# Plot via RGB
 plotRGB(sen, 1, 2, 3, stretch="Lin")
 nir <- sen[[1]]
-sd3 <- focal(nir, matrix(1/9, 3, 3), fun=sd) #sd = standand deviation
-# plotting with ggplot2
+sd3 <- focal(nir, matrix(1/9, 3, 3), fun=sd) # sd = standand deviation
+
+# Plotting with ggplot2
 ggplot() +
   geom_raster(sd3, mapping=aes(x = x, y = y, fill = layer))
-# error, we need a dataframe
+# Error, we need a dataframe
+
+# Create the dataframe, then we can use ggplot2
 sd3d <- as.data.frame(sd3, xy=T)
 ggplot() +
   geom_raster(sd3d, mapping=aes(x = x, y = y, fill = layer))
-  # with viridis
+
+# Now we can change the color and give a title to our plot
+# Viridis
 ggplot() +
 geom_raster(sd3d, mapping =aes(x=x, y=y, fill=layer)) +
 scale_fill_viridis() +
 ggtitle("Standard deviation by viridis package")
 
-# cividis
+# Cividis
 ggplot() +
 geom_raster(sd3d, mapping =aes(x=x, y=y, fill=layer)) +
 scale_fill_viridis(option = "cividis") +
 ggtitle("Standard deviation by viridis package")
 
-# magma
+# Magma
 ggplot() +
 geom_raster(sd3d, mapping =aes(x=x, y=y, fill=layer)) +
 scale_fill_viridis(option = "magma") +
 ggtitle("Standard deviation by viridis package")
 
-# inferno
+# Inferno
 ggplot() +
 geom_raster(sd3d, mapping =aes(x=x, y=y, fill=layer)) +
 scale_fill_viridis(option = "inferno") +
 ggtitle("Standard deviation by viridis package")
 
-# patchwork
+# Use patchwork package to combine separate ggplots into the same graphic
 p1 <- ggplot() +
 geom_raster(sd3d, mapping =aes(x=x, y=y, fill=layer)) +
 scale_fill_viridis() +
@@ -55,7 +69,7 @@ ggtitle("Standard deviation via the inferno colour scale")
 
 p1 + p2
 
-# Excersise: plot the original image (NIR) and it's standard deviation
+# Excersise: plot the original image (NIR) and its standard deviation
 nird <- as.data.frame(nir, xy=T)
 p3 <- ggplot() +
   geom_raster(nird, mapping =aes(x=x, y=y, fill=sentinel_1)) +
