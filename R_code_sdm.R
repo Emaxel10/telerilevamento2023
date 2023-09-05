426c09f42bb26b9c4041e89e100f6bc9d960d20a
Emaxel10 <127427373+Emaxel10@users.noreply.github.com>
2023-09-05 19:09:43 +0200

    Update R_code_sdm.R

--- R_code_sdm.R
+++ R_code_sdm.R
@@ -1,60 +1,74 @@
 # R code for species distribution modeling
+
 install.packages("sdm")
 library(sdm)
 install.packages("rgdal", dependencies=T)
 library(rgdal)
 library(raster)
+
+# Store, into "file", the data downloaded as shapefile (shp)
 file <- system.file("external/species.shp", package="sdm")
 file
 species <- shapefile(file)
 species
-# 0 assenza, 1 presenza
+
+# 0 absence, 1 presence
 plot(species, pch=19)
-# rappresentano tutte le occorrenze, sia 0 che 1
-# ora plottiamo solo le presenze
-presences <- species[species$Occurrence == 1, ] # faccio un campione
+
+# Represent all occurrences, both 0 and 1
+# Now we only plot the presences
+presences <- species[species$Occurrence == 1, ] # Let's make a sample
 plot(presences, col="blue", pch=19)
 absences <- species[species$Occurrence == 0, ]
 plot(absences, col="red", pch=19)
-# plottiamoli insieme
+
+# Plot them together
 plot(presences, col="blue", pch=19)
 points(absences, col="red", pch=19)
-# predittori, variabili ambientali 
+
+# Predictors, environmental variables
 path <- system.file("external", package = "sdm")
-# importiamo i predittori insieme
+
+# Importing the predictors
 list <- list.files(path=path, pattern = 'asc$', full.names = T)
 list
 preds <- stack(list)
 preds
 plot(preds)
-# usiamo una color ramp palette
+
+# Using a color Ramp Palette
 cl <- colorRampPalette(c('blue','orange','red','yellow')) (100)
 plot(preds, col=cl)
-# plot predictors and occurrences
+
+# Plot predictors and occurrences
 plot(preds$elevation, col=cl)
 points(species[species$Occurrence == 1,], pch=16)
-# usiamo la temperatura ora
+
+# Using temperature
 plot(preds$temperature, col=cl)
 points(species[species$Occurrence == 1,], pch=16)
-# precipitation
+
+# Precipitation
 plot(preds$precipitation, col=cl)
 points(species[species$Occurrence == 1,], pch=16)
-# vegetation
+
+# Vegetation
 plot(preds$vegetation, col=cl)
 points(species[species$Occurrence == 1,], pch=16)
 
-# modello lineare
-# spiegare al modello i dati da utilizzare
-# i dati a terra si chiamano training data
+# Linear model
+# Explain to the model the data to use
+# Ground data is called training data
 datasdm <- sdmData(train=species, predictors=preds)
 datasdm
-# alt 1 2 6 per la tilde
 m1 <- sdm(Occurrence ~ elevation + precipitation + temperature + vegetation, data=datasdm, methods = "glm")
-# raster output layer
+
+# Raster output layer
 p1 <- predict(m1, newdata=preds)
 plot(p1, col=cl)
 points(species[species$Occurrence == 1, ], pch=16)
-# aggiungiamo allo stack
+
+# Add to the stack
 s1 <- stack(preds,p1)
 names(s1)[5] <- "model"
 plot(s1, col=cl)
